84b4a40049e12fef8d2349941232bfa943ed2041
DODDAKA NAVEEN KUMAR <65757392+DODDAKA@users.noreply.github.com>
2024-10-19 04:12:24 +0000

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -6,13 +6,7 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-dnk"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "tfstatestacc02"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "terraformstatecontainer"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "network.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
-
+  
 }
 
 provider "azurerm" {
